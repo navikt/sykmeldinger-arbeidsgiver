03b3de9f359d8941b56b6fea229e5c08de83b55a
andreasnilsen <danduras@gmail.com>
2020-12-18 16:32:19 +0100

    first commit

---
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -0,0 +1,23 @@
+package no.nav.syfo
+
+import io.prometheus.client.hotspot.DefaultExports
+import no.nav.syfo.application.ApplicationServer
+import no.nav.syfo.application.ApplicationState
+import no.nav.syfo.application.createApplicationEngine
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+val log: Logger = LoggerFactory.getLogger("no.nav.syfo.sykmeldinger-arbeidsgiver")
+
+fun main() {
+    val env = Environment()
+    DefaultExports.initialize()
+    val applicationState = ApplicationState()
+    val applicationEngine = createApplicationEngine(
+        env,
+        applicationState
+    )
+    val applicationServer = ApplicationServer(applicationEngine, applicationState)
+    applicationServer.start()
+    applicationState.ready = true
+}
