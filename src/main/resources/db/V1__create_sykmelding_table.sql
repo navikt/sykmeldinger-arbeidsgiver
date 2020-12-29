6d60bb34526f28b016d41ebf03af7b93200bf104
andreasnilsen <danduras@gmail.com>
2020-12-29 10:26:55 +0100

    add leder_id

--- src/main/resources/db/V1__create_sykmelding_table.sql
+++ src/main/resources/db/V1__create_sykmelding_table.sql
@@ -2,6 +2,8 @@ CREATE TABLE sykmelding (
     sykmelding_id VARCHAR primary key not null,
     pasient_fnr VARCHAR not null,
     orgnummer VARCHAR not null,
+    juridisk_orgnummer VARCHAR null,
     timestamp TIMESTAMP with time zone not null,
+    leder_id VARCHAR null,
     sykmelding JSONB not null
 )
