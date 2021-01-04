5ea8b9206a1647a6851b23d58ec51bf7530fbe3b
andreasnilsen <danduras@gmail.com>
2021-01-04 22:02:32 +0100

    Read from kafka and save in db

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -1,5 +1,8 @@
 package no.nav.syfo
 
+import no.nav.syfo.kafka.KafkaConfig
+import no.nav.syfo.kafka.KafkaCredentials
+
 data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
     val applicationName: String = getEnvVar("NAIS_APP_NAME", "sykmeldinger-arbeidsgiver"),
@@ -8,10 +11,24 @@ data class Environment(
     val dbHost: String = getEnvVar("NAIS_DATABASE_HOST"),
     val dbPort: String = getEnvVar("NAIS_DATABASE_PORT"),
     val dbName: String = getEnvVar("NAIS_DATABASE_DATABASE"),
+    override val cluster: String = getEnvVar("NAIS_CLUSTER_NAME"),
+    override val kafkaBootstrapServers: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL"),
+    override val truststore: String? = getEnvVar("NAV_TRUSTSTORE_PATH"),
+    override val truststorePassword: String? = getEnvVar("NAV_TRUSTSTORE_PASSWORD"),
+    val syfoSendtSykmeldingTopic: String = "syfo-sendt-sykmelding"
 
-) {
+) : KafkaConfig {
     fun jdbcUrl(): String {
         return "jdbc:postgresql://$dbHost:$dbPort/$dbName"
+    }
+
+    fun kafkaCredentials(): KafkaCredentials {
+        return object : KafkaCredentials {
+            override val kafkaPassword: String
+                get() = getEnvVar("SERVICEUSER_PASSWORD")
+            override val kafkaUsername: String
+                get() = getEnvVar("SERVICEUSER_USERNAME")
+        }
     }
 }
 
--- src/main/kotlin/no/nav/syfo/sykmelding/kafka/SykmeldingConsumer.kt
+++ src/main/kotlin/no/nav/syfo/sykmelding/kafka/SykmeldingConsumer.kt
@@ -1,14 +1,18 @@
 package no.nav.syfo.sykmelding.kafka
 
+import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import no.nav.syfo.application.ApplicationState
 import no.nav.syfo.application.database.DatabaseInterface
+import no.nav.syfo.log
 import no.nav.syfo.sykmelding.db.deleteSykmelding
 import no.nav.syfo.sykmelding.db.insertOrUpdateSykmelding
 import no.nav.syfo.sykmelding.kafka.model.SendtSykmeldingKafkaMessage
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.clients.consumer.KafkaConsumer
 import java.time.Duration
+import java.time.Instant
 
 class SykmeldingConsumer(
     private val kafkaConsumer: KafkaConsumer<String, SendtSykmeldingKafkaMessage?>,
@@ -16,15 +20,43 @@ class SykmeldingConsumer(
     private val applicationState: ApplicationState,
     private val topic: String,
 ) {
+
+    private var lastLogTime = Instant.now().toEpochMilli()
+    private val logTimer = 60_000L
+
+    fun startConsumer() {
+        GlobalScope.launch {
+            try {
+                start()
+            } catch (ex: Exception) {
+                log.error("Error running kafka consumer", ex)
+            }
+        }
+    }
+
     suspend fun start() {
         kafkaConsumer.subscribe(listOf(topic))
+        var processedMessages = 0
+
         while (applicationState.ready) {
             val sykmeldinger = kafkaConsumer.poll(Duration.ZERO)
             sykmeldinger.forEach {
                 handleSykmelding(it)
             }
+            processedMessages += sykmeldinger.count()
+            processedMessages = logProcessedMessages(processedMessages)
             delay(1)
         }
+    }
+
+    private fun logProcessedMessages(processedMessages: Int): Int {
+        var currentLogTime = Instant.now().toEpochMilli()
+        if (processedMessages > 0 && currentLogTime - lastLogTime > logTimer) {
+            log.info("Processed $processedMessages messages")
+            lastLogTime = currentLogTime
+            return 0
+        }
+        return processedMessages
     }
 
     private fun handleSykmelding(consumerRecord: ConsumerRecord<String, SendtSykmeldingKafkaMessage?>) {
