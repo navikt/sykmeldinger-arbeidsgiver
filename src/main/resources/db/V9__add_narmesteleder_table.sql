f3f6d038ca75a79ff3ca74a5dd1d9083dbabf9f8
andreasnilsen <danduras@gmail.com>
2021-10-19 14:25:00 +0200

    add narmesteledertable

---
+++ src/main/resources/db/V9__add_narmesteleder_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE narmesteleder (
+    narmeste_leder_id VARCHAR primary key not null,
+    pasient_fnr VARCHAR not null,
+    leder_fnr VARCHAR not null,
+    orgnummer VARCHAR not null
+);
