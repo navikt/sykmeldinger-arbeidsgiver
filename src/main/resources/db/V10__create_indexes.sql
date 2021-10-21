8361f157bd00768d726035d6ec2138995c2ef15f
andreasnilsen <danduras@gmail.com>
2021-10-21 08:53:12 +0200

    Create indexs

---
+++ src/main/resources/db/V10__create_indexes.sql
@@ -0,0 +1,3 @@
+create index sykmelding_arbeidsgiver_orgnummer_idx on sykmelding_arbeidsgiver(orgnummer);
+create index narmesteleder_orgnummer_idx on narmesteleder(orgnummer);
+create index narmesteleder_pasient_fnr_idx on narmesteleder(pasient_fnr);
