3166b87e949c3564d91556c5315392e55fed23bf
Tia Firing <tia.firing@nav.no>
2022-05-03 14:11:31 +0200

    fjerner rester av onprem-kafka

--- src/main/kotlin/no/nav/syfo/Environment.kt
+++ src/main/kotlin/no/nav/syfo/Environment.kt
@@ -1,8 +1,6 @@
 package no.nav.syfo
 
 import com.nimbusds.jose.jwk.RSAKey
-import no.nav.syfo.kafka.KafkaConfig
-import no.nav.syfo.kafka.KafkaCredentials
 
 data class Environment(
     val applicationPort: Int = getEnvVar("APPLICATION_PORT", "8080").toInt(),
@@ -12,11 +10,7 @@ data class Environment(
     val dbHost: String = getEnvVar("NAIS_DATABASE_HOST"),
     val dbPort: String = getEnvVar("NAIS_DATABASE_PORT"),
     val dbName: String = getEnvVar("NAIS_DATABASE_DATABASE"),
-    override val cluster: String = getEnvVar("NAIS_CLUSTER_NAME"),
-    override val kafkaBootstrapServers: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS_URL"),
-    override val truststore: String? = getEnvVar("NAV_TRUSTSTORE_PATH"),
-    override val truststorePassword: String? = getEnvVar("NAV_TRUSTSTORE_PASSWORD"),
-    val syfoSendtSykmeldingTopic: String = "syfo-sendt-sykmelding",
+    val cluster: String = getEnvVar("NAIS_CLUSTER_NAME"),
     val syfoSendtSykmeldingTopicAiven: String = "teamsykmelding.syfo-sendt-sykmelding",
     val loginserviceIdportenDiscoveryUrl: String = getEnvVar("LOGINSERVICE_IDPORTEN_DISCOVERY_URL"),
     val loginserviceIdportenAudience: List<String> = getEnvVar("LOGINSERVICE_IDPORTEN_AUDIENCE").split(","),
@@ -31,18 +25,9 @@ data class Environment(
     val tokenXWellKnownUrl: String = getEnvVar("TOKEN_X_WELL_KNOWN_URL"),
     val clientIdTokenX: String = getEnvVar("TOKEN_X_CLIENT_ID"),
     val tokenXPrivateJwk: RSAKey = RSAKey.parse(getEnvVar("TOKEN_X_PRIVATE_JWK"))
-) : KafkaConfig {
+) {
     fun jdbcUrl(): String {
         return "jdbc:postgresql://$dbHost:$dbPort/$dbName"
-    }
-
-    fun kafkaCredentials(): KafkaCredentials {
-        return object : KafkaCredentials {
-            override val kafkaPassword: String
-                get() = getEnvVar("SERVICEUSER_PASSWORD")
-            override val kafkaUsername: String
-                get() = getEnvVar("SERVICEUSER_USERNAME")
-        }
     }
 }
 
