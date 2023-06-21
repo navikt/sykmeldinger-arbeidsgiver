74b76746f1cbcd4cd31ce629dc1726a9df1f4134
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-06-21 06:16:27 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ plugins {
     kotlin("jvm") version "1.8.22"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     id("org.hidetake.swagger.generator") version "2.19.2" apply true
+    id("org.cyclonedx.bom") version "1.7.4"
 }
 
 buildscript {
