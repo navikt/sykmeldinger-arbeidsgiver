d424b681ae8f6507a4854776faa53023821b87a0
Joakim Taule Kartveit <joakim.taule.kartveit@nav.no>
2023-10-26 15:09:17 +0200

    Added in secureLogs

--- src/main/kotlin/no/nav/syfo/Bootstrap.kt
+++ src/main/kotlin/no/nav/syfo/Bootstrap.kt
@@ -49,6 +49,7 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 val log: Logger = LoggerFactory.getLogger("no.nav.syfo.sykmeldinger-arbeidsgiver")
+val securelog: Logger = LoggerFactory.getLogger("securelog")
 
 @DelicateCoroutinesApi
 fun main() {
